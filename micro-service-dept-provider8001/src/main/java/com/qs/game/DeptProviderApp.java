ce14f79cc136ab499449101d1279f75ea2f54bf5
zunzhuowei <1808862723@qq.com>
2018-05-20 00:20:03 +0800

    rename

---
+++ micro-service-dept-provider8001/src/main/java/com/qs/game/DeptProviderApp.java
@@ -0,0 +1,18 @@
+package com.qs.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zun.wei on 2018/5/19.
+ * To change this template use File|Default Setting
+ * |Editor|File and Code Templates|Includes|File Header
+ */
+@SpringBootApplication
+public class DeptProviderApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProviderApp.class, args);
+}
+
+}
