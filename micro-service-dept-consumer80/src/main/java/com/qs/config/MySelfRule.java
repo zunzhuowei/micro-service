fa79ceefe48c7dbf1fc837cc137ab22ac4fc0119
zunzhuowei <1808862723@qq.com>
2018-05-26 22:06:11 +0800

    修改注释

--- micro-service-dept-consumer80/src/main/java/com/qs/config/MySelfRule.java
+++ micro-service-dept-consumer80/src/main/java/com/qs/config/MySelfRule.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * 指定已负载均衡算法，每个服务访问五次换下一个。
+ * 自定义负载均衡算法，每个服务访问五次换下一个。
  */
 @Configuration
 public class MySelfRule {
